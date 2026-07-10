b8e7534939f6919f5c37438a3f5759d135a46588
thamashijayawardana1997-stack <thamashijayawardana1997@gmail.com>
2026-07-11 00:39:22 +0530

    update the Added donor navigation sidebar

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,36 +1,3 @@
-import { useNavigate } from "react-router-dom";
-import { auth } from "../firebase";
-import { signOut } from "firebase/auth";
-
-function Navbar() {
-  const navigate = useNavigate();
-
-  const handleLogout = async () => {
-    await signOut(auth);
-    navigate("/login");
-  };
-
-  return (
-    <nav className="navbar">
-      <div className="navbar-brand" onClick={() => navigate("/home")}>
-        LifeLink 💉
-      </div>
-      <div className="navbar-links">
-        <button onClick={() => navigate("/home")}>Home</button>
-        <button onClick={() => navigate("/view-requests")}>
-          View Requests
-        </button>
-        <button onClick={() => navigate("/post-request")}>
-          Post Request
-        </button>
-        <button onClick={handleLogout}>Logout</button>
-      </div>
-    </nav>
-  );
-}
-
-export default Navbar;
-
 import React, { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { auth, db } from "../firebase";
@@ -139,3 +106,5 @@ function Navbar() {
     </div>
   );
 }
+
+export default Navbar;
